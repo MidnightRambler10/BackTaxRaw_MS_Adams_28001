7d52957a3f56c147c5183d12243d3483ab06a30e
Absher Saleem <abshir.saleem@lumentumllc.com>
2026-08-20 18:51:08 -0400

    baack to basic

--- BackTaxRaw_MS_Adams_28001/TaxSaleHistory.cs
+++
@@ -1,9 +0,0 @@
-namespace BackTaxRaw_MS_Adams_28001
-{
-    public class TaxSaleHistory
-    {
-        public string Year { get; set; } = string.Empty;
-        public string SoldTo { get; set; } = string.Empty;
-        public string RedeemedDateBy { get; set; } = string.Empty;
-    }
-}
