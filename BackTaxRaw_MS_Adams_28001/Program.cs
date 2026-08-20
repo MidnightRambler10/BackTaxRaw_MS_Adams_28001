1de153471e9a4a07458d9d36b3f8d178c25c672f
absher.saleem <absher.saleem@DataServer>
2026-08-21 00:14:37 +0500

    Rewrite Program.cs with explicit Main method

--- BackTaxRaw_MS_Adams_28001/Program.cs
+++ BackTaxRaw_MS_Adams_28001/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace BackTaxRaw_MS_Adams_28001
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
