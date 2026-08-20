50c74d95e16ba1e554b3b756206fef59f042ad05
absher.saleem <absher.saleem@DataServer>
2026-08-21 03:12:02 +0500

    Complete Adams County lien scraper pipeline

---
+++ BackTaxRaw_MS_Adams_28001/TaxSaleHistory.cs
@@ -0,0 +1,9 @@
+namespace BackTaxRaw_MS_Adams_28001
+{
+    public class TaxSaleHistory
+    {
+        public string Year { get; set; } = string.Empty;
+        public string SoldTo { get; set; } = string.Empty;
+        public string RedeemedDateBy { get; set; } = string.Empty;
+    }
+}
