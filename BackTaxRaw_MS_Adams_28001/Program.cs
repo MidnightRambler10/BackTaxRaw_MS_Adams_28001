17fecd57916fd1c2e710c4995b32834e067e480e
absher.saleem <absher.saleem@DataServer>
2026-08-21 18:35:56 +0500

    Save successful liens to database

--- BackTaxRaw_MS_Adams_28001/Program.cs
+++ BackTaxRaw_MS_Adams_28001/Program.cs
@@ -77,6 +77,8 @@ namespace BackTaxRaw_MS_Adams_28001
                 }
             }
 
+            SaveToDB(successfulLiens);
+
             if (errorList.Count > 0)
             {
                 StringBuilder body = new StringBuilder();
@@ -634,6 +636,164 @@ namespace BackTaxRaw_MS_Adams_28001
                 TAX_INFORMATION =
                     taxInformation
             };
+        }
+
+
+
+
+        private static void SaveToDB(List<LienResult> successfulLiens)
+        {
+            const string connectionString =
+                "Server=DataServer;Database=ContentGrabber;Trusted_Connection=True;TrustServerCertificate=True;";
+
+            const string recreateTableQuery = @"
+                DROP TABLE IF EXISTS dbo.SuccessfulLiens;
+
+                CREATE TABLE dbo.SuccessfulLiens
+                (
+                    TaxLienID           NVARCHAR(255) NULL,
+                    APN                 NVARCHAR(255) NULL,
+                    AdvertisementNumber NVARCHAR(255) NULL,
+                    ParcelID            NVARCHAR(255) NULL,
+                    AuctionYear         NVARCHAR(255) NULL,
+                    DelinquentYear      NVARCHAR(255) NULL,
+                    PARCEL              NVARCHAR(MAX) NULL,
+                    ADDRESS             NVARCHAR(MAX) NULL,
+                    OWNER               NVARCHAR(MAX) NULL,
+                    ACRES               NVARCHAR(MAX) NULL,
+                    LAND_VALUE          NVARCHAR(MAX) NULL,
+                    IMPROVEMENTS        NVARCHAR(MAX) NULL,
+                    TOTAL_VALUE         NVARCHAR(MAX) NULL,
+                    ASSESSED            NVARCHAR(MAX) NULL,
+                    PPIN                NVARCHAR(MAX) NULL,
+                    TOWNSHIP            NVARCHAR(MAX) NULL,
+                    LEGAL               NVARCHAR(MAX) NULL,
+                    TAX_DISTRICT        NVARCHAR(MAX) NULL,
+                    SECTION             NVARCHAR(MAX) NULL,
+                    RANGE               NVARCHAR(MAX) NULL,
+                    TAX_YEAR            NVARCHAR(MAX) NULL,
+                    RECORDS_LAST_UPDATED NVARCHAR(MAX) NULL,
+                    EXEMPT_CODE         NVARCHAR(MAX) NULL,
+                    HOMESTEAD_CODE      NVARCHAR(MAX) NULL,
+                    BOOK                NVARCHAR(MAX) NULL,
+                    PAGE                NVARCHAR(MAX) NULL,
+                    TAX_INFO_YEAR       NVARCHAR(MAX) NULL,
+                    COUNTY_TAX_DUE      NVARCHAR(MAX) NULL,
+                    COUNTY_PAID         NVARCHAR(MAX) NULL,
+                    COUNTY_BALANCE      NVARCHAR(MAX) NULL,
+                    CITY_TAX_DUE        NVARCHAR(MAX) NULL,
+                    CITY_PAID           NVARCHAR(MAX) NULL,
+                    CITY_BALANCE        NVARCHAR(MAX) NULL,
+                    SCHOOL_TAX_DUE      NVARCHAR(MAX) NULL,
+                    SCHOOL_PAID         NVARCHAR(MAX) NULL,
+                    SCHOOL_BALANCE      NVARCHAR(MAX) NULL,
+                    TOTAL_TAX_DUE       NVARCHAR(MAX) NULL,
+                    TOTAL_PAID          NVARCHAR(MAX) NULL,
+                    TOTAL_BALANCE       NVARCHAR(MAX) NULL,
+                    LAST_PAYMENT_DATE   NVARCHAR(MAX) NULL
+                );";
+
+            const string insertQuery = @"
+                INSERT INTO dbo.SuccessfulLiens
+                (
+                    TaxLienID, APN, AdvertisementNumber, ParcelID, AuctionYear,
+                    DelinquentYear, PARCEL, ADDRESS, OWNER, ACRES, LAND_VALUE,
+                    IMPROVEMENTS, TOTAL_VALUE, ASSESSED, PPIN, TOWNSHIP, LEGAL,
+                    TAX_DISTRICT, SECTION, RANGE, TAX_YEAR, RECORDS_LAST_UPDATED,
+                    EXEMPT_CODE, HOMESTEAD_CODE, BOOK, PAGE, TAX_INFO_YEAR,
+                    COUNTY_TAX_DUE, COUNTY_PAID, COUNTY_BALANCE, CITY_TAX_DUE,
+                    CITY_PAID, CITY_BALANCE, SCHOOL_TAX_DUE, SCHOOL_PAID,
+                    SCHOOL_BALANCE, TOTAL_TAX_DUE, TOTAL_PAID, TOTAL_BALANCE,
+                    LAST_PAYMENT_DATE
+                )
+                VALUES
+                (
+                    @TaxLienID, @APN, @AdvertisementNumber, @ParcelID, @AuctionYear,
+                    @DelinquentYear, @PARCEL, @ADDRESS, @OWNER, @ACRES, @LAND_VALUE,
+                    @IMPROVEMENTS, @TOTAL_VALUE, @ASSESSED, @PPIN, @TOWNSHIP, @LEGAL,
+                    @TAX_DISTRICT, @SECTION, @RANGE, @TAX_YEAR, @RECORDS_LAST_UPDATED,
+                    @EXEMPT_CODE, @HOMESTEAD_CODE, @BOOK, @PAGE, @TAX_INFO_YEAR,
+                    @COUNTY_TAX_DUE, @COUNTY_PAID, @COUNTY_BALANCE, @CITY_TAX_DUE,
+                    @CITY_PAID, @CITY_BALANCE, @SCHOOL_TAX_DUE, @SCHOOL_PAID,
+                    @SCHOOL_BALANCE, @TOTAL_TAX_DUE, @TOTAL_PAID, @TOTAL_BALANCE,
+                    @LAST_PAYMENT_DATE
+                );";
+
+            using SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            using SqlTransaction transaction = connection.BeginTransaction();
+
+            try
+            {
+                using (SqlCommand recreateCommand =
+                    new SqlCommand(recreateTableQuery, connection, transaction))
+                {
+                    recreateCommand.ExecuteNonQuery();
+                }
+
+                foreach (LienResult result in successfulLiens)
+                {
+                    using SqlCommand insertCommand =
+                        new SqlCommand(insertQuery, connection, transaction);
+
+                    void AddValue(string name, string? value)
+                    {
+                        insertCommand.Parameters.Add(name, SqlDbType.NVarChar, -1).Value =
+                            string.IsNullOrWhiteSpace(value) ? DBNull.Value : value;
+                    }
+
+                    AddValue("@TaxLienID", result.Lien.TaxLienID);
+                    AddValue("@APN", result.Lien.APN);
+                    AddValue("@AdvertisementNumber", result.Lien.AdvNum);
+                    AddValue("@ParcelID", result.Lien.ParcelID);
+                    AddValue("@AuctionYear", result.Lien.AuctionYear);
+                    AddValue("@DelinquentYear", result.Lien.TaxYear);
+                    AddValue("@PARCEL", result.PARCEL);
+                    AddValue("@ADDRESS", result.ADDRESS);
+                    AddValue("@OWNER", result.OWNER);
+                    AddValue("@ACRES", result.ACRES);
+                    AddValue("@LAND_VALUE", result.LAND_VALUE);
+                    AddValue("@IMPROVEMENTS", result.IMPROVEMENTS);
+                    AddValue("@TOTAL_VALUE", result.TOTAL_VALUE);
+                    AddValue("@ASSESSED", result.ASSESSED);
+                    AddValue("@PPIN", result.PPIN);
+                    AddValue("@TOWNSHIP", result.TOWNSHIP);
+                    AddValue("@LEGAL", result.LEGAL);
+                    AddValue("@TAX_DISTRICT", result.TAX_DISTRICT);
+                    AddValue("@SECTION", result.SECTION);
+                    AddValue("@RANGE", result.RANGE);
+                    AddValue("@TAX_YEAR", result.TAX_YEAR);
+                    AddValue("@RECORDS_LAST_UPDATED", result.RECORDS_LAST_UPDATED);
+                    AddValue("@EXEMPT_CODE", result.EXEMPT_CODE);
+                    AddValue("@HOMESTEAD_CODE", result.HOMESTEAD_CODE);
+                    AddValue("@BOOK", result.BOOK);
+                    AddValue("@PAGE", result.PAGE);
+                    AddValue("@TAX_INFO_YEAR", result.TAX_INFORMATION?.YEAR);
+                    AddValue("@COUNTY_TAX_DUE", result.TAX_INFORMATION?.COUNTY_TAX_DUE);
+                    AddValue("@COUNTY_PAID", result.TAX_INFORMATION?.COUNTY_PAID);
+                    AddValue("@COUNTY_BALANCE", result.TAX_INFORMATION?.COUNTY_BALANCE);
+                    AddValue("@CITY_TAX_DUE", result.TAX_INFORMATION?.CITY_TAX_DUE);
+                    AddValue("@CITY_PAID", result.TAX_INFORMATION?.CITY_PAID);
+                    AddValue("@CITY_BALANCE", result.TAX_INFORMATION?.CITY_BALANCE);
+                    AddValue("@SCHOOL_TAX_DUE", result.TAX_INFORMATION?.SCHOOL_TAX_DUE);
+                    AddValue("@SCHOOL_PAID", result.TAX_INFORMATION?.SCHOOL_PAID);
+                    AddValue("@SCHOOL_BALANCE", result.TAX_INFORMATION?.SCHOOL_BALANCE);
+                    AddValue("@TOTAL_TAX_DUE", result.TAX_INFORMATION?.TOTAL_TAX_DUE);
+                    AddValue("@TOTAL_PAID", result.TAX_INFORMATION?.TOTAL_PAID);
+                    AddValue("@TOTAL_BALANCE", result.TAX_INFORMATION?.TOTAL_BALANCE);
+                    AddValue("@LAST_PAYMENT_DATE", result.TAX_INFORMATION?.LAST_PAYMENT_DATE);
+
+                    insertCommand.ExecuteNonQuery();
+                }
+
+                transaction.Commit();
+            }
+            catch
+            {
+                transaction.Rollback();
+                throw;
+            }
         }
 
 
